ff38f7b73fc057c9db0ee645daf1cb9561ec089a
binnu6757 <reddysab11@gmail.com>
2024-08-01 16:43:42 +0530

    Update edetection.py

--- edetection.py
+++ edetection.py
@@ -6,7 +6,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Display title
-st.image(r"C:\Users\reddy\OneDrive\Desktop\new logo.jpg")  # Replace with your actual PNG image file path
+st.image(r"new logo.jpg")  # Replace with your actual PNG image file path
 
 
 # Display the PNG image
@@ -15,8 +15,8 @@ st.write('This app performs emotional analysis Distribution.')
 
 
 try:
-    model = pickle.load(open(r"C:\Users\reddy\machine learning\ss.pkl",'rb'))
-    bow = pickle.load(open(r"C:\Users\reddy\machine learning\bow1.pkl",'rb'))
+    model = pickle.load(open(r"ss.pkl",'rb'))
+    bow = pickle.load(open(r"bow1.pkl",'rb'))
 except Exception as e:
     st.error(f"An error occurred: {e}")
 
@@ -36,19 +36,19 @@ if user_input:
 if st.button("Submit"):
     if prediction == 0:
         st.write("Sad")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\sad.jpeg")
+        st.image(r"sad.jpeg")
     elif prediction == 1:
         st.write("Joy")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\joy.jpeg")
+        st.image(r"joy.jpeg")
     elif prediction == 2:
         st.write("Love")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\love.jpeg")
+        st.image(r"love.jpeg")
     elif prediction == 3:
         st.write("Anger")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\anger.png")
+        st.image(r"anger.png")
     elif prediction == 4:
         st.write("Fear")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\fear.jpeg")
+        st.image(r"fear.jpeg")
     elif prediction == 5:
         st.write("Surprise")
-        st.image(r"C:\Users\reddy\OneDrive\Desktop\surprised.png")
+        st.image(r"surprised.png")
